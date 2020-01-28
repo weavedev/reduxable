5807eaa81dc54b5ba8634d2cddfc4586c289daf8
Paul Gerarts <paul@gerarts.nl>
2020-01-28 21:11:47 +0100

    Standardise actions into actionMap object

--- lib/Reduxable.ts
+++ lib/Reduxable.ts
@@ -8,11 +8,15 @@ export interface ReduxableState<T = any> {
 
 export type InternalReducer<S = any> = (state: S, action: Action) => S;
 
+export interface ActionMap {
+    [key: string]: AnyAction;
+}
+
 /**
  * Abstract something something
  */
 export abstract class Reduxable<S extends ReduxableState, P extends any[] = []> {
-    public abstract readonly actions: AnyAction;
+    public abstract readonly actionMap: ActionMap;
     public abstract readonly defaultState: S;
     public abstract readonly saga: Saga;
     public abstract readonly runSaga: (...arg: P) => SagaIterator<S>;
@@ -20,6 +24,10 @@ export abstract class Reduxable<S extends ReduxableState, P extends any[] = []> 
     protected abstract readonly internalReducer: InternalReducer<S>;
 
     private internalState?: S;
+
+    public get actions(): ActionMap[keyof ActionMap] {
+        throw new Error('ReduxAsync.actions should only be used as a TypeScript type provider (typeof .actions)');
+    }
 
     public abstract run(...arg: P): AnyAction;
 
